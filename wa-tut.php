7e232186dfd61d8877670b86fe7b6aef411d107a
HAN <macbookair@HAN-3.local>
2026-06-07 13:55:49 +0700

    ok

--- wa-tut.php
+++ wa-tut.php
@@ -1,9 +1,9 @@
 <?php
-error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-// Coba tes apakah PHP jalan dengan menambahkan ini:
-echo "PHP sedang berjalan!";
+require_once 'auth_checkwa.php';
 require_once 'config.php';
 
 
