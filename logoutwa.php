40fe9eded36582fa431ead12c93be57896a9ed92
HAN <macbookair@HAN-3.local>
2026-06-07 12:36:51 +0700

    ok

--- logoutwa.php
+++ logoutwa.php
@@ -1,10 +1,13 @@
 <?php
 session_start();
-require_once 'config.php';
+// Hapus atau beri komentar require_once jika tidak diperlukan di proses logout
+// require_once 'config.php'; 
 
 $_SESSION['logout_message'] = "Berhasil logout!";
 session_unset();
 session_destroy();
 
-header("Location: loginwa.php");
+// MENGHINDARI LOGIN DI DALAM IFRAME: Paksa seluruh halaman (top window) kembali ke login
+echo "<script>window.top.location.href = 'loginwa.php';</script>";
 exit();
+?>
