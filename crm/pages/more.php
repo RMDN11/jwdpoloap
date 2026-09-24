40cdcdca52f583b637d035115836a50e4db9b9ca
RMDN11 <hanrmdn11@gmail.com>
2026-09-24 18:32:12 +0700

    feat: link More to CRM Group broadcast

--- crm/pages/more.php
+++ crm/pages/more.php
@@ -3,7 +3,7 @@
 <div class="action-list">
     <a href="../manage_templates.php"><i class="fa-regular fa-file-lines"></i><div><strong>Template Pesan</strong><span>Kelola template pesan</span></div><i class="fa-solid fa-chevron-right"></i></a>
     <a href="../manage_auto_reply.php"><i class="fa-solid fa-robot"></i><div><strong>Auto Reply</strong><span>Atur balasan otomatis</span></div><i class="fa-solid fa-chevron-right"></i></a>
-    <a href="../kelola_grup.php"><i class="fa-solid fa-users"></i><div><strong>Kelola Grup</strong><span>Daftar dan pengelolaan grup</span></div><i class="fa-solid fa-chevron-right"></i></a>
+    <a href="?page=group"><i class="fa-solid fa-bullhorn"></i><div><strong>Kirim ke Grup</strong><span>Broadcast dan jadwal grup</span></div><i class="fa-solid fa-chevron-right"></i></a><a href="../kelola_grup.php"><i class="fa-solid fa-users"></i><div><strong>Kelola Grup</strong><span>Daftar dan pengelolaan grup</span></div><i class="fa-solid fa-chevron-right"></i></a>
     <a href="../grafik.php"><i class="fa-solid fa-chart-line"></i><div><strong>Analytics</strong><span>Lihat analitik CRM</span></div><i class="fa-solid fa-chevron-right"></i></a>
     <a href="logout.php"><i class="fa-solid fa-right-from-bracket danger"></i><div><strong>Keluar</strong><span>Logout akun</span></div><i class="fa-solid fa-chevron-right"></i></a>
 </div>
