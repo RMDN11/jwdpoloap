f51dfefe7b0fe9fcc893756658c0be2581864c62
RMDN11 <hanrmdn11@gmail.com>
2026-09-25 17:25:03 +0700

    feat: route Pengingat Peserta to CRM workspace

--- crm/pages/reminder.php
+++ crm/pages/reminder.php
@@ -7,7 +7,7 @@ $tabs = [
     ['label' => 'Pembayaran', 'icon' => 'fa-wallet', 'href' => '?page=reminder-pembayaran', 'desc' => 'Pengingat pembayaran peserta'],
     ['label' => 'Pengajar', 'icon' => 'fa-chalkboard-user', 'href' => '?page=reminder-pengajar', 'desc' => 'Kirim pesan kepada pengajar'],
     ['label' => 'Promosi', 'icon' => 'fa-bullhorn', 'href' => '?page=reminder-promosi', 'desc' => 'Kirim promosi dan broadcast peserta'],
-    ['label' => 'Pengingat Peserta', 'icon' => 'fa-clock', 'href' => '../kelola_reminder.php', 'desc' => 'Kelola reminder dan follow-up peserta'],
+    ['label' => 'Pengingat Peserta', 'icon' => 'fa-clock', 'href' => '?page=reminder-peserta', 'desc' => 'Kelola reminder dan follow-up peserta'],
 ];
 ?>
 
