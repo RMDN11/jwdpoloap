4802c875f091daec65d59ebb52ded6acd0edbb96
RMDN11 <hanrmdn11@gmail.com>
2026-09-26 10:14:32 +0700

    fix: authenticate Chat polling through CRM bootstrap

--- crm/actions/chat-poll.php
+++ crm/actions/chat-poll.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../config/prospect.php';
+require_once __DIR__ . '/../config/bootstrap.php';
 require_once __DIR__ . '/../config/chat.php';
 
 header('Content-Type: application/json; charset=utf-8');
